bbb482b8efee6989587ab34497a51e5ed26d9bca
Nick Morgan <nick.morgan@coinbase.com>
2022-09-01 18:46:47 -0400

    Add license comment

--- dba/migrations/000007_initialize_test_accounts.up.sql
+++ dba/migrations/000007_initialize_test_accounts.up.sql
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2022 Coinbase Global, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 INSERT INTO account (id, portfolio_id, user_id, currency, created_at)
 VALUES('B183F5E2-B72A-4AA5-B7AE-95E0D548D84D','D263E7E3-24D7-4C04-8D67-EA3A0BE7907E','620E62FD-DAF1-4738-84CE-1DBC4393ED29','wei',now());
 
