e13a47d5b63f69e2a76b17169274ace9971226d3
Nick Morgan <nick.morgan@coinbase.com>
2022-09-01 15:08:26 -0400

    Add GRPC Health Check

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/reflection"
 
 	api "LedgerApp/protos/ledger"
@@ -33,6 +35,11 @@ func main() {
 	server := grpc.NewServer(
 		grpc.Creds(tlsCredentials),
 	)
+
+	//setup health server
+	healthServer := health.NewServer()
+	healthServer.SetServingStatus("grpc.health.v1.Health", grpc_health_v1.HealthCheckResponse_SERVING)
+	grpc_health_v1.RegisterHealthServer(server, healthServer)
 
 	api.RegisterLedgerServer(server, service)
 	reflection.Register(server)
